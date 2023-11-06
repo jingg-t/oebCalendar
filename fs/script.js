ff5aa2d7554b4509014d4f6d369515776f330d5a
Jingyang Tang <115130139+jingg-t@users.noreply.github.com>
2023-11-05 21:07:51 -0500

    Add files via upload

---
+++ fs/script.js
@@ -0,0 +1,167 @@
+const date = new Date();
+
+const { google } = require('googleapis');
+
+const CLIENT_ID = '322375598882-9h6rdghg9g99bcun5lgd3f98cqn4n9o1.apps.googleusercontent.com';
+const CLIENT_secret = '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';
+const REDIRECT_URI = '';
+
+const novMatchDays = new Set([ // NOVEMBER hardcoded set of exhibition match days
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    5,
+    4
+  ])
+
+  const decMatchDays = new Set([ // DECEMBER hardcoded set of exhibition match days
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22
+])
+
+const janMatchDays = new Set([ // JANUARY hardcoded set of exhibition match days
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26
+])
+
+const novMatchDetails = ([]);
+
+const renderCalendar = () => {
+
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days"); //change the days 
+  
+  // Specific dates 
+  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(); 
+  const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate(); 
+  const firstDayIndex = date.getDay();
+  const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay(); 
+  const nextDays = 7 - lastDayIndex - 1
+  const months = [ // used for al the months
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+  
+  // write month and date dynamically 
+  document.querySelector('.date h1').innerHTML = months[date.getMonth()]
+  document.querySelector('.date p').innerHTML = new Date().toDateString();
+  
+  let days = "";
+  
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date"> ${prevLastDay - x + 1}</div>`
+  }
+  
+  for (let i = 1; i <= lastDay; i++) { // print days from beginning to end
+    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+      days += `<div class="today">${i}</div>`; // when elements are today
+    } else if (novMatchDays.has(i) && date.getMonth() == 10){ // hard coded month of exhibition matches
+      days += `<div class="matchDay" id="${i}" onclick="renderDetails(this.id)"><u><strong>${i}</strong></u></div>`;
+    } else if (decMatchDays.has(i) && date.getMonth() == 11){ // hard coded month of exhibition matches
+      days += `<div class="matchDay" id="${i}" onclick="renderDetails(this.id)"><u><strong>${i}</strong></u></div>`;
+    } else if (janMatchDays.has(i) && date.getMonth() == 0){ // hard coded month of exhibition matches
+      days += `<div class="matchDay" id="${i}" onclick="renderDetails(this.id)"><u><strong>${i}</strong></u></div>`;
+    } else { 
+      days += `<div>${i}</div>`;
+    }
+  }
+  
+  for (let j = 1; j <= nextDays; j++){
+    days += `<div class="next-date">${j}</div>`
+    monthDays.innerHTML = days;
+  }
+  
+  
+
+}
+
+
+//open form 
+document.querySelector('.days').addEventListener('click', () =>{ 
+  document.getElementById("myForm").style.display = "block";    
+})
+
+//WRITE TO FORM HERE 
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function renderDetails(matchID) { // show the details of the match
+  console.log(matchID); //ADD JSON READ FILES 
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+  date.setMonth(date.getMonth() - 1)
+  renderCalendar()
+})
+
+document.querySelector('.next').addEventListener('click', () => {
+  date.setMonth(date.getMonth() + 1)
+  renderCalendar()
+})
+
+
+renderCalendar()
